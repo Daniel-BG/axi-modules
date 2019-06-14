cc582a9e1d079b9cf1c89747b9a8be9ef0e53162
Daniel-BG <danielbasgar@gmail.com>
2019-06-14 12:21:12 +0200

    Cleanup unused functions

--- src/pkg/am_functions.vhd
+++ src/pkg/am_functions.vhd
@@ -1,28 +1,13 @@
 package am_functions is
 	--headers
-	--function bits(invalue: integer) return integer;
 	function am_minval(a, b: integer) return integer;
 	function am_maxval(a, b: integer) return integer;
-	--function sum(a: integer_vector) return integer;
-	--function partsum(a: integer_vector; numelems: integer) return integer;
 	function am_unsignedlen(invalue: integer; is_signed: boolean) return integer;
 	
 end am_functions;
 
 package body am_functions is
---	--actual function bodies
---	function bits(invalue: integer) return integer is
---		variable i: integer := 1;
---	begin
---		while i <= 32 loop
---			if invalue <= 2**i - 1 then
---				return i;
---			end if;
---			i := i + 1;
---		end loop;
---		return -1;
---	end function;
-
+	--actual function bodies
 	function am_minval(a, b: integer) return integer is
 	begin
 		if a > b then
@@ -40,24 +25,6 @@ package body am_functions is
 			return b;
 		end if;
 	end function;
-
---	function sum(a: integer_vector) return integer is
---		variable res: integer := 0;
---	begin
---		for i in a'low to a'high loop
---			res := res + a(i);
---		end loop;
---		return res;
---	end function;
-	
---	function partsum(a: integer_vector; numelems: integer) return integer is
---		variable res: integer := 0;
---	begin
---		for i in a'low to a'low + numelems - 1 loop
---			res := res + a(i);
---		end loop;
---		return res;
---	end function;
 
     function am_unsignedlen(invalue: integer; is_signed: boolean) return integer is
     begin
